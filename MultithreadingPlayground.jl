45b0be908928bbfa7a7fc370adbe337f7f01b300
Cvndas <eidasmedia@outlook.com>
2026-06-03 19:43:50 +0200

    Refactored the multithreded pseudoimplementation

--- MultithreadingPlayground.jl
+++ MultithreadingPlayground.jl
@@ -4,7 +4,7 @@ using Base.Threads
 
 function SleepRandomForThreadTesting()
     min = 0.000005
-    max = 0.00005
+    max = 0.05
     sleepTime = rand() * (max - min) + min
     sleep(sleepTime)
     # println("Done sleeping")
@@ -12,8 +12,8 @@ end
 
 pathfindingA_Count = 0
 pathfindingB_Count = 0
-pathfindingA_Necessary = rand(10:1000_000)
-pathfindingB_Necessary = rand(100:1000_000)
+pathfindingA_Necessary = rand(10:100)
+pathfindingB_Necessary = rand(30:130)
 function pseudo_Pathfinding_PathA()
     global pathfindingA_Necessary
     global pathfindingA_Count
@@ -56,14 +56,15 @@ function PseudoWorkerCore()
     p.pathATilesReady[] = true
     p.pathBTilesReady[] = true
 
-
     println("Going to spawn the MPI thread")
     @spawn pseudo_WorkerMPI(p)
     println("Going to start working")
     while p.readyToStart[] == false
+        yield()
     end
-    pseudo_WorkerWorker(p)
+    pseudo_WorkerPathfinder(p)
 end
+
 
 mutable struct Pseudo_WorkerCore
     isDone::Threads.Atomic{Bool}
@@ -86,66 +87,47 @@ mutable struct Pseudo_WorkerCore
 end
 
 
+# Utilizing the fact that Atomic{} is a reference type, to refer to specific variables from the other struct
+mutable struct Pseudo_WorkerWorkerState
+    isPathA::Bool
+    pathDone::Bool
+    pathTilesNecessary::Threads.Atomic{Bool}
+    pathTilesReady::Threads.Atomic{Bool}
+end
 
 
 function pseudo_AddProductionTilesToWorkingTiles()
     SleepRandomForThreadTesting()
 end
-
-function pseudo_WorkerWorker(p::Pseudo_WorkerCore)
+function pseudo_PrintPathProgress(w::Pseudo_WorkerWorkerState)
+    # These two are just for this pseudo implementation. Won't be part of the true implementation
     global pathfindingA_Count
     global pathfindingB_Count
-    pathADone = false
-    pathBDone = false
+    if w.isPathA
+        println("Path A wasnt done yet. Progress: $pathfindingA_Count")
+    else
+        println("Path B wasnt done yet. Progress: $pathfindingB_Count")
+    end
+end
+
+function pseudo_Pathfind(w::Pseudo_WorkerWorkerState)
+    if w.isPathA
+        return pseudo_Pathfinding_PathA()
+    else
+        return pseudo_Pathfinding_PathB()
+    end
+end
+
+
+function pseudo_WorkerPathfinder(p::Pseudo_WorkerCore)
+
+    a = Pseudo_WorkerWorkerState(true, false, p.pathATilesNecessary, p.pathATilesReady)
+    b = Pseudo_WorkerWorkerState(false, false, p.pathBTilesNecessary, p.pathBTilesReady)
 
     while true
-        if pathADone == false
-            while p.pathATilesReady[] == false
-                # Busy waiting for the tiles to be ready. 
-                yield()
-            end
-
-            if p.pathATilesNecessary[]
-                # println("Worker: PathA Tiles were necessary, so we're gonna lock production tiles and read them ")
-                lock(p.lock_ProductionTiles)
-                pseudo_AddProductionTilesToWorkingTiles()
-                # println("Worker: PathA tiles were processed. Unlocking production tiles lock now")
-                unlock(p.lock_ProductionTiles)
-            end
-            pathADone = pseudo_Pathfinding_PathA()
-        end
-        if pathADone == false
-            println("PathA wasnt done yet. Progress: $pathfindingA_Count")
-            lock(p.lock_MakeSupplementRequest)
-            p.pathATilesReady[] = false
-            p.pathATilesNecessary[] = true
-            notify(p.cond_MakeSupplementRequest)
-            unlock(p.lock_MakeSupplementRequest)
-        end
-
-        if pathBDone == false
-            while p.pathBTilesReady[] == false
-                # Busy waiting for the tiles to be ready
-                yield()
-            end
-
-            if p.pathBTilesNecessary[]
-                lock(p.lock_ProductionTiles)
-                pseudo_AddProductionTilesToWorkingTiles()
-                unlock(p.lock_ProductionTiles)
-            end
-            pathBDone = pseudo_Pathfinding_PathB()
-        end
-        if pathBDone == false
-            println("PathB wasnt done yet. Progress: $pathfindingB_Count")
-            lock(p.lock_MakeSupplementRequest)
-            p.pathBTilesReady[] = false
-            p.pathBTilesNecessary[] = true
-            notify(p.cond_MakeSupplementRequest)
-            unlock(p.lock_MakeSupplementRequest)
-        end
-
-        bothDone = pathADone && pathBDone
+        pseudo_WorkerPathfind(p, a)
+        pseudo_WorkerPathfind(p, b)
+        bothDone = a.pathDone && b.pathDone
         p.isDone[] = bothDone
         if (bothDone)
             lock(p.lock_MakeSupplementRequest)
@@ -157,6 +139,32 @@ function pseudo_WorkerWorker(p::Pseudo_WorkerCore)
     end
 end
 
+function pseudo_WorkerPathfind(p::Pseudo_WorkerCore, w::Pseudo_WorkerWorkerState)
+    if w.pathDone == false
+        while w.pathTilesReady[] == false
+            yield()
+        end
+
+        if w.pathTilesNecessary[]
+            lock(p.lock_ProductionTiles)
+            pseudo_AddProductionTilesToWorkingTiles()
+            unlock(p.lock_ProductionTiles)
+        end
+        w.pathDone = pseudo_Pathfind(w)
+    end
+    if w.pathDone == false
+        pseudo_PrintPathProgress(w)
+        lock(p.lock_MakeSupplementRequest)
+        w.pathTilesReady[] = false
+        w.pathTilesNecessary[] = true
+        notify(p.cond_MakeSupplementRequest)
+        unlock(p.lock_MakeSupplementRequest)
+    end
+end
+
+
+
+
 function pseudo_SendSupplementRequest()
     SleepRandomForThreadTesting()
 end
@@ -166,32 +174,45 @@ function pseudo_WaitAndProcessMapSupplement()
     return [1]
 end
 
-function pseudo_WorkerMPI(p::Pseudo_WorkerCore)
+function pseudo_MapSupplementIsInTheMail()
+    return rand(1:10) > 5
+end
 
+
+#=
+Reasoning about why we can't interlink reading map supplements from master with reading map supplement REQUESTS from worker: 
+Doing the wait() again while we are waiting for a supplement to come in is not just expensive, but
+incorrect. Then we're hoping that the worker is gonna wake us up with another "please request another supplement". 
+If that doesn't come, of it the supplement is for the same path as the currently pending one, we're overwriting business.
+Basically, when any request (or both) is made, we HAVE to serve back a map supplement before accepting more requests.
+=#
+
+function pseudo_WorkerMPI(p::Pseudo_WorkerCore)
+    # Prelude
     waitingForSupplement_A = false
     waitingForSupplement_B = false
+    # The correctness of this algorithn depends on us having this lock at all times, except for when we call wait.
     lock(p.lock_MakeSupplementRequest)
     p.readyToStart[] = true
-    # println("Producer: successfully acquired the initial lock on MakeSupplementRequest")
-    while true
-        # println("Producer: LOOOOP : Before the isDoneCheck")
-        if p.isDone[]
-            break
-        end
 
-        if waitingForSupplement_A
-            lock(p.lock_ProductionTiles)
-            p.productionTiles = pseudo_WaitAndProcessMapSupplement()
-            p.pathATilesReady[] = true
-            unlock(p.lock_ProductionTiles)
-            waitingForSupplement_A = false
-        end
-        if waitingForSupplement_B
-            lock(p.lock_ProductionTiles)
-            p.productionTiles = pseudo_WaitAndProcessMapSupplement()
-            p.pathBTilesReady[] = true
-            unlock(p.lock_ProductionTiles)
-            waitingForSupplement_B = false
+    while true
+        while waitingForSupplement_A || waitingForSupplement_B
+            # Busy wait, only because we don't have this hooked up to MPI. Otherwise, we woud IProbe()
+            if waitingForSupplement_A && pseudo_MapSupplementIsInTheMail()
+                lock(p.lock_ProductionTiles)
+                p.productionTiles = pseudo_WaitAndProcessMapSupplement()
+                p.pathATilesReady[] = true
+                unlock(p.lock_ProductionTiles)
+                waitingForSupplement_A = false
+            end
+            if waitingForSupplement_B && pseudo_MapSupplementIsInTheMail()
+                lock(p.lock_ProductionTiles)
+                p.productionTiles = pseudo_WaitAndProcessMapSupplement()
+                p.pathBTilesReady[] = true
+                unlock(p.lock_ProductionTiles)
+                waitingForSupplement_B = false
+            end
+            yield()
         end
 
         # We're either woken up by worker wanting us to make a map request, or because worker is done.
@@ -212,7 +233,6 @@ function pseudo_WorkerMPI(p::Pseudo_WorkerCore)
             waitingForSupplement_B = true
             p.pathBTilesNecessary[] = false
             pseudo_SendSupplementRequest()
-
         end
     end
 
